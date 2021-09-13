18fd5d5af0129d3c03c79e44e961749fd4a2d57f
Gavin Noronha <gavinln@hotmail.com>
2021-09-12 20:34:13 -0700

    clean up dash examples

--- python/dash/app-bar-graph.py
+++ python/dash/app-bar-graph.py
@@ -1,58 +1,53 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+from dash import dcc
+from dash import html
+
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
+colors = {"background": "#111111", "text": "#7FDBFF"}
 
-style_center = {
-    'textAlign': 'center',
-    'colors': colors['text']
-}
+style_center = {"textAlign": "center", "colors": colors["text"]}
 
-app.layout = html.Div(style={
-    'backgroundColor': colors['background']}, children=[
-
-    html.H1(children='Hello Dash', style=style_center),
-
-    html.Div(children='''
+app.layout = html.Div(
+    style={"backgroundColor": colors["background"]},
+    children=[
+        html.H1(children="Hello Dash", style=style_center),
+        html.Div(
+            children="""
         Dash: A web application framework for Python.
-    ''', style=style_center),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {
-                    'x': [1, 2, 3],
-                    'y': [4, 1, 2],
-                    'type': 'bar',
-                    'name': 'SF'
+    """,
+            style=style_center,
+        ),
+        dcc.Graph(
+            id="example-graph",
+            figure={
+                "data": [
+                    {
+                        "x": [1, 2, 3],
+                        "y": [4, 1, 2],
+                        "type": "bar",
+                        "name": "SF",
+                    },
+                    {
+                        "x": [1, 2, 3],
+                        "y": [2, 4, 5],
+                        "type": "bar",
+                        "name": u"Montréal",
+                    },
+                ],
+                "layout": {
+                    "title": "Dash Data Visualization",
+                    "plot_bgcolor": colors["background"],
+                    "paper_bgcolor": colors["background"],
+                    "font": {"color": colors["text"]},
                 },
-                {
-                    'x': [1, 2, 3],
-                    'y': [2, 4, 5],
-                    'type': 'bar',
-                    'name': u'Montréal'
-                },
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization',
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
-        }
-    )]
+            },
+        ),
+    ],
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
